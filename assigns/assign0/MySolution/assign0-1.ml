dee553ac78e971af7c8a2c658beb88860148efa0
Wyatt <wnapier@bu.edu>
2023-09-09 16:49:49 -0400

    finished; double check testing

--- assigns/assign0/MySolution/assign0-1.ml
+++ assigns/assign0/MySolution/assign0-1.ml
@@ -1,29 +1,11 @@
+(* factorial function *)
 let rec
 fact(x: int): int =
 if x > 0 then x * fact(x-1) else 1
 ;;
 
-(* ****** ****** *)
-
-let fact10 = fact(10)
-;;
-let fact100 = fact(100)
-;;
-let fact 1M = fact(1000000000)
-;;
-
-(* ****** ****** *)
-
+(* ****** loop that calls fact() until it returns 0 ****** *)
 let rec
-myloop(x: int): int = 
-if fact(x) = 0 then x else myloop(x+1)
-;;
-
-(* 
-MacCarthy's 91-function to test compilers
-should always return 91
-*)
-let rec
-f91(x: int): int =
-if x > 100 then x - 10 else f91(f91(x+11))
+myloop(n: int): int = 
+if fact(n) = 0 then n else myloop(n+1)
 ;;
