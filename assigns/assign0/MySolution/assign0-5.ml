f96d103b14d39426c45c73c6ec6a35fe44298eb2
Wyatt <wnapier@bu.edu>
2023-09-10 18:10:37 -0400

    code completed - passes test

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -3,10 +3,33 @@
    hella recurison
 *)
 
+(* ****** ****** *)
+
+let chr = Char.chr
+let ord = Char.code
+let str(c0) = String.make 1 c0
+;;
+(* ****** ****** *)
+
+let string_init = String.init
+let string_length = String.length
+let string_get(cs, i0) = String.get cs i0
+;;
+(* ****** ****** *)
+
 let rec
-f(i: int): char = ...
+helper (old: string) (rev: string): string = 
+   if string_length old = string_length rev then rev
+   else
+      helper(old) (string_init (string_length rev + 1) (fun i -> 
+         if i == 0 then string_get(old, string_length rev)
+         else string_get(rev, (i - 1))
+      ))
 ;;
 
-f(0) = 'H'
-f(1) = 'e'
-...
+let
+stringrev(cs: string): string = 
+   helper (cs) ""
+;;
+
+(* stringrev("hello");; *)
