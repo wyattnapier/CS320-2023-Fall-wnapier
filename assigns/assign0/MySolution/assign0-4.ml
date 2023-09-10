4c67116b53b12486b7cd84f645af77e697e0f7e9
Wyatt <wnapier@bu.edu>
2023-09-10 16:18:01 -0400

    additional error handling and commenting

--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -18,26 +18,28 @@ let string_get(cs, i0) = String.get cs i0
 ;;
 (* ****** ****** *)
 
-(* always set sum to 1 to start *)
+(* 
+raises a base to a power
+@params base, the power its raised to, and product (should default to 1)
+@returns an int
+*)
 let rec power (base: int) (expo: int) (prod: int): int =
    if expo = 0 then prod
    else power (base) (expo-1) (prod*base)
    ;;
 
+(* 
+   helper function for str 2 int
+   handles negative symbol and checks if input string is valid then constructs int from string
+   @params sum is final result (initally 1), s = string; count is number of repetitions (initially 1)
+   @returns an int from the string
+*)
 let rec helper (sum: int) (s: string) (count: int): int =
    if count <= 0 then sum
    else if ord(string_get(s, count-1)) = 45 then sum * -1
+   else if ord(string_get(s, count-1)) < 48 || ord(string_get(s, count-1)) > 58 then invalid_arg s
    else
       helper (sum + power (10) (string_length s - count) (ord(string_get(s, count-1)) -48)) (s) (count - 1) (* flips order *)
-      (* helper (sum + (power (10) (count) (1) * (ord(string_get(s, count-1))))-48) (s) (count+1) *)
-
-      (* 
-         THIS WORKS FOR SUMMING DIGITS
-         helper (sum + ord(string_get(s, count-1)) -48) (s) (count + 1) *)
-      (* 
-         THIS WORKS FOR GETTING ONE CHAR
-         ord(string_get(s, count-1)) -48  
-      *)
 ;;
 
 let str2int(cs: string): int =
