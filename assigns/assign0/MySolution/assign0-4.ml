e5d92b4f7d5d93f25d7d59c4ff4c3793d0013b38
Wyatt <wnapier@bu.edu>
2023-09-10 12:42:03 -0400

    adding case for negative

--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -19,16 +19,25 @@ let string_get(cs, i0) = String.get cs i0
 (* ****** ****** *)
 
 (* always set sum to 1 to start *)
-let rec power (base: int) (expo: int) (sum: int): int =
-   if expo = 0 then sum
-   else power (base) (expo-1) (sum*base)
+let rec power (base: int) (expo: int) (prod: int): int =
+   if expo = 0 then prod
+   else power (base) (expo-1) (prod*base)
    ;;
 
 let rec helper (sum: int) (s: string) (count: int): int =
    if count > string_length (s) then sum
-   else 
-      helper (sum + (power (10) (count) (1) * (ord(string_get(s, count-1))))-48) (s) (count+1)
-   (* else let increase = get_string(count) in helper *)
+   else if ord(string_get(s, count-1)) = 45 then sum * -1
+   else
+      helper (sum + power (10) (count-1) (ord(string_get(s, count-1)) -48)) (s) (count + 1) (* flips order *)
+      (* helper (sum + (power (10) (count) (1) * (ord(string_get(s, count-1))))-48) (s) (count+1) *)
+
+      (* 
+         THIS WORKS FOR SUMMING DIGITS
+         helper (sum + ord(string_get(s, count-1)) -48) (s) (count + 1) *)
+      (* 
+         THIS WORKS FOR GETTING ONE CHAR
+         ord(string_get(s, count-1)) -48  
+      *)
 ;;
 
 let str2int(cs: string): int =
