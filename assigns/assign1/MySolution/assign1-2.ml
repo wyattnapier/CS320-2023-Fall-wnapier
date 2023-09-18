deaa1df67f0139749338dd5630fc1e6f13bb5e66
Wyatt <wnapier@bu.edu>
2023-09-18 14:14:56 -0400

    problem solved

--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -1,35 +1,15 @@
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
 
-(* concat string directly and then *)
-
 (* use string_make_fwork *)
 let string_merge(cs1)(cs2): string = 
-  (* merge strings using a list *)
-  let list_cs1 = 
+  if string_length cs1 = 0 then cs2 else if string_length cs2 = 0 then cs1
+  else 
+    let combolen = string_length cs1 + string_length cs2 in
+    let rec building (p1: int) (p2: int) (currString) = 
+      if string_length currString = combolen then currString
+      else if string_length cs1 <= p1 then building p1 (p2+1) (string_make_fwork (fun work -> (string_foreach(currString)(work); work(string_get_at cs2 p2))))
+      else if string_length cs2 <= p2 then building (p1+1) p2 (string_make_fwork (fun work -> (string_foreach(currString)(work); work(string_get_at cs1 p1))))
+      else if string_get_at cs1 p1 < string_get_at cs2 p2 then building (p1+1) p2 (string_make_fwork (fun work -> (string_foreach(currString)(work); work(string_get_at cs1 p1))))
+      else building p1 (p2+1) (string_make_fwork (fun work -> (string_foreach(currString)(work); work(string_get_at cs2 p2))))
+    in building 0 0 "";
 ;;
-
-(* ****************** ********************* *)
-
-(* APPROACH MERGING TWO STRINGS THEN SORTING *)
-  (* merge strings *)
-  (* let result =
-    string_make_fwork
-    ( fun work -> 
-      (string_foreach(cs1)(work); string_foreach(cs2)(work))
-    ) in
-    (* sort result *)
-    string_foreach(C) *)
-
-(* ****************** ********************* *)
-
-(* APPROACH USING FOR LOOP AND APPENDING SMALLER VALUE EACH TIME *)
-  (* string_make_fwork
-  ( fun work -> 
-    (* instead of 'b' I need to compare to the value of the curr char in cs2 then take whatever is lower *)
-    string_foreach(cs1)
-    (fun c ->
-      if c >= 'b' then work(c)
-      else work('z')
-    )
-  ) *)
-    
