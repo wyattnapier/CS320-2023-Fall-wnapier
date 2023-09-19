3609316a88a0c5d52d2f133cc975e55ecae9fb6b
Wyatt <wnapier@bu.edu>
2023-09-19 17:20:33 -0400

    OH progress

--- assigns/assign1/MySolution/assign1-1.ml
+++ assigns/assign1/MySolution/assign1-1.ml
@@ -1,18 +1,25 @@
 (* #use "./../../../classlib/OCaml/MyOCaml.ml";; *)
+(* breaks for numbers with 0 in the middle *)
+
+let rec int_length (digits: int) (len_counter: int): int = 
+  if digits mod 10 > 0 then int_length(digits/10)(len_counter+1)
+  else len_counter
+
+let rec pow (base: int) (expo: int) (prod: int): int =
+  if expo <= 0 then prod
+  else pow (base) (expo-1) (prod*base)
+
+let rec int_each (input: int)(n_length:int) (acc: int) (count: int) (prev0: bool): int = 
+  if input > 0 then int_each (input / 10)(n_length) (acc + (input mod 10) * pow(10) (n_length - count) (1)) (count + 1) false
+    (* if input mod 10 = 0 then int_each (input/10)(n_length) (acc) (count + 1) true
+    else 
+      if prev0 then int_each (input / 10)(n_length) (acc + (input mod 10) * pow(10) (n_length - count + 1) (1)) (count + 1) false
+      else int_each (input / 10)(n_length) (acc + (input mod 10) * pow(10) (n_length - count) (1)) (count + 1) false why does n_length - count - 1 need the -1? *)
+  else acc
 
 let intrev10(n: int): int =
-  let rec int_length (digits: int) (len_counter: int): int = 
-    if digits mod 10 > 0 then int_length(digits/10)(len_counter+1)
-    else len_counter
-  in
-  let n_length = int_length(n) (0) in
-  let rec pow (base: int) (expo: int) (prod: int): int =
-    if expo = 0 then prod
-    else pow (base) (expo-1) (prod*base)
-  in
-  let rec int_each (input: int) (acc: int) (count: int): int = 
-    if input > 0 then 
-      int_each (input / 10) (acc + (input mod 10) * pow(10) (n_length - count - 1) (1)) (count + 1) (* why does n_length - count - 1 need the -1? *)
-    else acc
-  in int_each(n)(0)(0) (* might need a 1 for the last arg*)
+  if n mod 10 = 0 then n
+  else
+    let n_length = int_length(n) (0) in
+    int_each(n)(n_length)(0)(1)false (* might need a 1 for the last arg*)
 ;;
--- assigns/assign1/MySolution/assign1-5.ml
+++ assigns/assign1/MySolution/assign1-5.ml
@@ -1,26 +1,23 @@
-(* if i != 0 && currChar > currChar - 1 then streak++ and if streak > max then max = streak and streakEndIndex = i;
-else i++;
-return substring of streak
-*)
-
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
 
 let char_str_concat (c: char) (str: string): string =
   string_init(string_length str + 1) (fun i -> if i = string_length str then c else string_get_at str (i))
+
+let char2int (c: char): int = 
+  ord c - ord '0'  (* Convert char to integer *)
+
+let int2char (n: int): char = 
+  chr(n + ord '0')
+
+let rec in_out (xs: string) (i: int) (subseq: string): string =
+  if i >= string_length xs then subseq
+  else if i = 0 || string_length subseq = 0 || ord (string_get_at xs i) >= ord (string_get_at subseq (string_length subseq - 1)) then 
+    let add = in_out xs (i + 1) (char_str_concat (string_get_at xs i) subseq) in
+    let exclude = in_out xs (i + 1) (subseq) in
+    if string_length add >= string_length exclude then add else exclude
+  else 
+    in_out xs (i + 1) (subseq)
+
 let string_longest_ascend(xs: string): string = 
-  let rec run_finder (i: int) (max_run: string) (new_run: string):string = 
-    if i >= string_length xs then
-      if string_length new_run > string_length max_run then new_run else max_run (* error when the two are same length - last one is one short *)
-    else 
-      let curr = string_get_at xs i in
-      let prev = if i = 0 then '0' else string_get_at xs (i - 1) in
-        if curr >= prev || i = 0 then 
-          let updated_new_run = char_str_concat curr new_run in
-          if string_length new_run > string_length max_run then
-            run_finder (i+1)(updated_new_run)(updated_new_run)
-          else
-            run_finder (i+1) (max_run)(updated_new_run)
-        else 
-          run_finder (i+1) (max_run)("")
-  in run_finder 0 "" ""
+  in_out xs 0 ""
 ;;
