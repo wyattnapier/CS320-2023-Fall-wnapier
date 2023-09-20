bf5341b68c8d2efadeb10d668e2f41349f5cf657
Wyatt <wnapier@bu.edu>
2023-09-20 10:17:26 -0400

    minor fix

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -5,18 +5,29 @@ integer to a string that represents the integer:
 fun int2str(i0: int): string
 *)
 
-#use "./../../../classlib/OCaml/MyOCaml.ml";;
+
+let chr = Char.chr
+let ord = Char.code
+let str(c0) = String.make 1 c0
+;;
+(* ****** ****** *)
+
+let string_init = String.init
+let string_length = String.length
+let string_get(cs, i0) = String.get cs i0
+;;
+(* ****** ****** *)
 
 let rec helper (n: int) (s: string): string =
    if n > 0 then
       helper (n/10) (string_init (string_length s + 1) (fun i -> 
       if i == 0 then chr((n mod 10) + 48) 
-      else string_get_at s (i-1)))
+      else string_get(s, (i-1))))
    else s
 ;;
 
 let int2str(i0: int): string = 
    if i0 = 0 then "0" 
-   else if i0 < 0 then let positive = helper(-1 * i0) ("") in string_init (string_length positive + 1) (fun i -> if i == 0 then '-' else string_get_at positive (i-1))
+   else if i0 < 0 then let positive = helper(-1 * i0) ("") in string_init (string_length positive + 1) (fun i -> if i == 0 then '-' else string_get (positive, (i-1)))
    else helper(i0) ("")
 ;;
