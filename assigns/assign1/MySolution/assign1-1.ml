6205aecc7912b18c2d8eecdd64a69e023274f2c3
Wyatt <wnapier@bu.edu>
2023-09-20 09:30:17 -0400

    restructuring assign1-1.ml

--- assigns/assign1/MySolution/assign1-1.ml
+++ assigns/assign1/MySolution/assign1-1.ml
@@ -1,25 +1,15 @@
-(* #use "./../../../classlib/OCaml/MyOCaml.ml";; *)
-(* breaks for numbers with 0 in the middle *)
-
-let rec int_length (digits: int) (len_counter: int): int = 
-  if digits mod 10 > 0 then int_length(digits/10)(len_counter+1)
-  else len_counter
-
-let rec pow (base: int) (expo: int) (prod: int): int =
-  if expo <= 0 then prod
-  else pow (base) (expo-1) (prod*base)
-
-let rec int_each (input: int)(n_length:int) (acc: int) (count: int) (prev0: bool): int = 
-  if input > 0 then int_each (input / 10)(n_length) (acc + (input mod 10) * pow(10) (n_length - count) (1)) (count + 1) false
-    (* if input mod 10 = 0 then int_each (input/10)(n_length) (acc) (count + 1) true
-    else 
-      if prev0 then int_each (input / 10)(n_length) (acc + (input mod 10) * pow(10) (n_length - count + 1) (1)) (count + 1) false
-      else int_each (input / 10)(n_length) (acc + (input mod 10) * pow(10) (n_length - count) (1)) (count + 1) false why does n_length - count - 1 need the -1? *)
-  else acc
-
+(* 
+WOOHOO I FIXED IT!! (I did have to basically restart though)
+the acc*10 opens the rightmost slot (the lowest digit) in acc
+num mod 10 fills it with rightmost digit of num
+num/10 chops off the rightmost digit for next iteration
+*)
 let intrev10(n: int): int =
-  if n mod 10 = 0 then n
+  if n < 10 then n
   else
-    let n_length = int_length(n) (0) in
-    int_each(n)(n_length)(0)(1)false (* might need a 1 for the last arg*)
+    let rec accumulator (acc: int) (num: int): int =
+      if num <= 0 then acc
+      else accumulator (acc*10 + num mod 10) (num/10)
+    in
+    accumulator 0 n
 ;;
