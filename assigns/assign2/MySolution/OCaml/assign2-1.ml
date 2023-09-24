51417ab0701d5f6deca41a170ee79b84a2c022fb
Wyatt <wnapier@bu.edu>
2023-09-24 15:12:56 -0400

    progress on assign2

--- assigns/assign2/MySolution/OCaml/assign2-1.ml
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -1,9 +1,11 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
 #use "./../../assign2.ml";;
 
-let rec
-mylist_length(xs: 'a mylist): int = 
+let rec mylist_length(xs: 'a mylist): int = 
 match xs with
 | MyNil -> 0
-| MyCons(x1, xs) -> 1 + mylist_length(xs)
+| MyCons(x1, xs) -> 1 + mylist_length(xs) (* works for these two *)
+| MySnoc(xs, x1) -> 1 + mylist_length(xs)
+| MyReverse(xs) -> mylist_length(xs)
+| MyAppend2(xs1, xs2) -> mylist_length(xs1) + mylist_length(xs2)
 ;;
--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -2,8 +2,10 @@
 #use "./../../assign2.ml";;
 #use "./assign2-1.ml";;
 
-let rec
-mylist_get_at(xs: 'a mylist)(i0: int): 'a =
+let rec mylist_get_at(xs: 'a mylist)(i0: int): 'a =
 match xs with
 | MyNil -> mylist_subscript_exn()
 | MyCons(x, xs) -> if i0 = 0 then x else mylist_get_at(xs)(i0-1)
+| MySnoc(xs, x1) -> if i0 = 0 then x1 else mylist_get_at(xs)(i0-1)
+| MyReverse(xs) -> mylist_get_at(xs)(i0)
+| MyAppend2(xs1, xs2) -> if mylist_length(xs1) <= i0 then mylist_get_at(xs2)(i0 - mylist_length(xs1)) else mylist_get_at(xs1)(i0)
