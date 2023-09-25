03873be043822adfada56f0b0b55e64cc1ed33e8
Wyatt <wnapier@bu.edu>
2023-09-25 17:30:48 -0400

    fixing assign2-2

--- assigns/assign2/MySolution/OCaml/assign2-1.ml
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -4,7 +4,7 @@
 let rec mylist_length(xs: 'a mylist): int = 
 match xs with
 | MyNil -> 0
-| MyCons(x1, xs) -> 1 + mylist_length(xs) (* works for these two *)
+| MyCons(x1, xs) -> 1 + mylist_length(xs)
 | MySnoc(xs, x1) -> 1 + mylist_length(xs)
 | MyReverse(xs) -> mylist_length(xs)
 | MyAppend2(xs1, xs2) -> mylist_length(xs1) + mylist_length(xs2)
