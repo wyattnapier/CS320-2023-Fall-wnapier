331459c87cb8e749f49d1aae087a95d526e7108f
Wyatt <wnapier@bu.edu>
2023-10-02 23:10:30 -0400

    3-4 completed

--- assigns/assign3/MySolution/OCaml/assign3-4.ml
+++ assigns/assign3/MySolution/OCaml/assign3-4.ml
@@ -25,6 +25,30 @@ https://xanadu-lang.github.io/xats2js/docgen/CodeBook/Doublet/2020-11-29/
 https://github.com/xanadu-lang/xats2js/tree/master/docgen/CodeBook/Doublet
 *)
 *)
-let list_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let string_alphabet = "abcdefghijklmnopqrstuvwxyz"
-list_make_fwork()
+
+(* function to change just one character and make it into a string using string and index *)
+(* do that for every character barring original character*)
+(* loop through each character in the string using other step function *)
+(* stick it all in the list *)
+
+(* takes the char that we're making buddies of and the current index that we're changing char for and the char we're replacing *)
+let replace_char(str: string)(i: int)(c: char): string =
+  string_tabulate(string_length str) (fun j -> 
+    if i = j then c else (string_get_at(str)(i))
+  )
+(* makes all strings of changing the nth char *)
+let generate_strings(str: string)(i: int): 'a list =
+  let string_alphabet = "abcdefghijklmnopqrstuvwxyz" in
+  list_make_fwork (fun work -> 
+      (int1_foreach (26) (fun j -> 
+        let curr_char = string_get_at str i in
+        let swap_char = string_get_at string_alphabet j in
+        if curr_char != swap_char then work(replace_char str i swap_char)
+      ))
+    )
+let
+list_of_buddies(word: string): string list = 
+  let res = list_make_fwork ( fun work ->
+    (int1_foreach (string_length word) (fun i -> work(generate_strings(word)(i))))) 
+  in list_concat (res)
+;;
