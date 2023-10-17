2bab0fe870d70f71eebac0427a9db3d7add2351c
Wyatt <wnapier@bu.edu>
2023-10-17 01:14:40 -0400

    work for tonight

--- assigns/assign4/MySolution/Python/assign4_6.py
+++ assigns/assign4/MySolution/Python/assign4_6.py
@@ -34,8 +34,52 @@
 # def theNatPairs_cubesum(): # please give your implementation
 #
 ################################################
-import sys
-sys.path.append("../../../../classlib/Python")
+
+###########################################################################
+
+# class strcon:
+#   ctag = -1
+#   def get_ctag(self):
+#       return self.ctag
+#   def __iter__(self):
+#       return strcon_iter(self)
+# # end-of-class(strcon)
+
+# class strcon_nil(strcon):
+#   def __init__(self):
+#       self.ctag = 0
+#       return None
+# # end-of-class(strcon_nil)
+
+# class strcon_cons(strcon):
+#   def __init__(self, cons1, cons2):
+#       self.ctag = 1
+#       self.cons1 = cons1
+#       self.cons2 = cons2
+#       return None
+#   def get_cons1(self):
+#       return self.cons1
+#   def get_cons2(self):
+#       return self.cons2
+# # end-of-class(strcon_cons)
+
+# class strcon_iter:
+#     def __iter__(self):
+#         return self
+#     def __init__(self, itms):
+#         self.itms = itms
+#     def __next__(self):
+#         if (self.itms.ctag==0):
+#             raise StopIteration
+#         else:
+#             itm1 = self.itms.cons1
+#             self.itms = self.itms.cons2()
+#             return itm1
+
+###########################################################################
+
+import sys 
+sys.path.append("./../../../../classlib/Python/")
 from MyPython import *
 
 def cube_sum(ij):
