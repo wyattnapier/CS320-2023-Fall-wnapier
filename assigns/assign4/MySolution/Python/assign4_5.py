e222e974fca7f60f5055004a18fe0107cae2cd70
Wyatt <wnapier@bu.edu>
2023-10-17 22:25:59 -0400

    fixing 5

--- assigns/assign4/MySolution/Python/assign4_5.py
+++ assigns/assign4/MySolution/Python/assign4_5.py
@@ -4,57 +4,90 @@
 # Please translate the following code from OCaml
 # into Python:
 #
-# let
-# string_fset_at
-# (cs: string)(i0: int)(c0: char) =
-# string_tabulate
-# (string_length(cs))
-# (
-# fun i ->
-# if i <> i0 then string_get_at(cs)(i) else c0)
+# DONE
+# let string_fset_at (cs: string)(i0: int)(c0: char) =
+# string_tabulate (string_length(cs)) (fun i ->
+#   if i <> i0 then string_get_at(cs)(i) else c0)
 # ;;
 # (* ****** ****** *)
 #
-# let
-# alphabet =
-# string_tabulate(26)(fun i -> chr(ord('a') + i));;
+# DONE
+# let alphabet = string_tabulate(26)(fun i -> chr(ord('a') + i));;
 #
 # (* ****** ****** *)
 #
-# let
-# list_of_buddies
-# (word: string): string list =
-# let n0 =
-# string_length(word) in
-# list_make_fwork
-# (
-# fun work ->
-# int1_foreach(n0)
-# (
-# fun i0 ->
-# let c0 =
-# string_get_at(word)(i0) in
-# string_foreach(alphabet)
-# (fun c1 -> if c1 <> c0 then work(string_fset_at(word)(i0)(c1)))))
+# let list_of_buddies (word: string): string list =
+# let n0 = string_length(word) in
+# list_make_fwork (fun work ->
+#    int1_foreach(n0) (fun i0 ->
+#       let c0 = string_get_at(word)(i0) in
+#       string_foreach(alphabet)(fun c1 -> if c1 <> c0 then work(string_fset_at(word)(i0)(c1)))))
 # ;; 
 # (* ****** ****** *)
 #
 #
 ################################################
 
-def string_fset_at(cs, i0, c0):
-  def tabulate(length):
-    return [c0 if i==i0 else cs[i] for i in range(length)]
-  return tabulate(len(cs))
+def string_length(str):
+  return len(str)
 
-#alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet = [chr(ord('a') + i) for i in range(26)]
+def string_get_at(str, i):
+  return str[i]
+
+def string_tabulate(n, fun):
+  str = ""
+  for i in range(n):
+    str = str + fun(i)
+  return str
+
+
+def string_fset_at(cs, i0, c0): 
+  return string_tabulate(string_length(cs), lambda i: string_get_at(cs, i) if i != i0 else c0)
+
+def alphabet():
+  return string_tabulate(26, lambda i: chr(ord('a') + i))
+
+#delete this later its in the library
+def int1_foreach(n0, work_func):
+    i0 = 0
+    while(i0 < n0):
+        work_func(i0)
+        i0 = (i0 + 1)
+    return None
+
+def fnlist_make_fwork(fwork):
+ res1 = []
+ fwork(lambda x: res1.append(x) if x != None else None)
+ return res1
+
+def string_foreach(str, fun):
+  int1_foreach(len(str), lambda i: fun(str[i]))
+
 
 def list_of_buddies(word):
-  def make_work(i0, c0):
-    return lambda string: string_fset_at(string, i0, c0)
+  n0 = string_length(word)
+  return fnlist_make_fwork(lambda work:\
+                           int1_foreach(n0, lambda i0:\
+                                string_foreach(alphabet(), lambda c1: \
+                                               work(string_fset_at(word, i0, c1) if c1 != string_get_at(word, i0) else None))))
+
+
+
+
+
+# def string_fset_at(cs, i0, c0):
+#   def tabulate(length):
+#     return [c0 if i==i0 else cs[i] for i in range(length)]
+#   return tabulate(len(cs))
+
+# #alphabet = "abcdefghijklmnopqrstuvwxyz"
+# alphabet = [chr(ord('a') + i) for i in range(26)]
+
+# def list_of_buddies(word):
+#   def make_work(i0, c0):
+#     return lambda string: string_fset_at(string, i0, c0)
   
-  def buddy_work(i0, c0):
-    return [work(word) for c1 in alphabet if c1 != c0 for work in [make_work(i0, c1)]]
+#   def buddy_work(i0, c0):
+#     return [work(word) for c1 in alphabet if c1 != c0 for work in [make_work(i0, c1)]]
   
-  return [buddy for i0, c0 in enumerate(word) for buddy in buddy_work(i0, c0)]
+#   return [buddy for i0, c0 in enumerate(word) for buddy in buddy_work(i0, c0)]
--- assigns/assign4/MySolution/Python/assign4_6.py
+++ assigns/assign4/MySolution/Python/assign4_6.py
@@ -1,117 +1,117 @@
-# Assign4-6:
-#
-# HX-2023-10-06: 30 points (bonus)
-#
-# (*
-# //
-# Please implement the following function
-# that enumerates all the pairs (i, j) of natural
-# numbers satisfying $i <= j$; a pair (i1, j1) must
-# be enumerated ahead of another pair (i2, j2) if the
-# following condition holds:
-#   i1*i1*i1 + j1*j1*j1 < i2*i2*i2 + j2*j2*j2
-# //
-# let
-# theNatPairs_cubesum(): (int * int) stream = fn () =>
-# //
-# *)
-#
-# Your implementation of theNatPairs_cubesum should
-# return a generator corresponding to the OCaml stream
-# If you have already implement a stream in Python, you
-# turn it into a generator by calling the following fun:
-#
-# def generator_of_stream(fxs):
-#     while True:
-#         cxs = fxs()
-#         if cxs.ctag == 0:
-#             break
-#         else:
-#             fxs = cxs.cons2
-#             yield cxs.cons1
-#     raise StopIteration
-#
-# def theNatPairs_cubesum(): # please give your implementation
-#
-################################################
+# # Assign4-6:
+# #
+# # HX-2023-10-06: 30 points (bonus)
+# #
+# # (*
+# # //
+# # Please implement the following function
+# # that enumerates all the pairs (i, j) of natural
+# # numbers satisfying $i <= j$; a pair (i1, j1) must
+# # be enumerated ahead of another pair (i2, j2) if the
+# # following condition holds:
+# #   i1*i1*i1 + j1*j1*j1 < i2*i2*i2 + j2*j2*j2
+# # //
+# # let
+# # theNatPairs_cubesum(): (int * int) stream = fn () =>
+# # //
+# # *)
+# #
+# # Your implementation of theNatPairs_cubesum should
+# # return a generator corresponding to the OCaml stream
+# # If you have already implement a stream in Python, you
+# # turn it into a generator by calling the following fun:
+# #
+# # def generator_of_stream(fxs):
+# #     while True:
+# #         cxs = fxs()
+# #         if cxs.ctag == 0:
+# #             break
+# #         else:
+# #             fxs = cxs.cons2
+# #             yield cxs.cons1
+# #     raise StopIteration
+# #
+# # def theNatPairs_cubesum(): # please give your implementation
+# #
+# ################################################
 
-###########################################################################
+# ###########################################################################
 
-# class strcon:
-#   ctag = -1
-#   def get_ctag(self):
-#       return self.ctag
-#   def __iter__(self):
-#       return strcon_iter(self)
-# # end-of-class(strcon)
+# # class strcon:
+# #   ctag = -1
+# #   def get_ctag(self):
+# #       return self.ctag
+# #   def __iter__(self):
+# #       return strcon_iter(self)
+# # # end-of-class(strcon)
 
-# class strcon_nil(strcon):
-#   def __init__(self):
-#       self.ctag = 0
-#       return None
-# # end-of-class(strcon_nil)
+# # class strcon_nil(strcon):
+# #   def __init__(self):
+# #       self.ctag = 0
+# #       return None
+# # # end-of-class(strcon_nil)
 
-# class strcon_cons(strcon):
-#   def __init__(self, cons1, cons2):
-#       self.ctag = 1
-#       self.cons1 = cons1
-#       self.cons2 = cons2
-#       return None
-#   def get_cons1(self):
-#       return self.cons1
-#   def get_cons2(self):
-#       return self.cons2
-# # end-of-class(strcon_cons)
+# # class strcon_cons(strcon):
+# #   def __init__(self, cons1, cons2):
+# #       self.ctag = 1
+# #       self.cons1 = cons1
+# #       self.cons2 = cons2
+# #       return None
+# #   def get_cons1(self):
+# #       return self.cons1
+# #   def get_cons2(self):
+# #       return self.cons2
+# # # end-of-class(strcon_cons)
 
-# class strcon_iter:
-#     def __iter__(self):
-#         return self
-#     def __init__(self, itms):
-#         self.itms = itms
-#     def __next__(self):
-#         if (self.itms.ctag==0):
-#             raise StopIteration
-#         else:
-#             itm1 = self.itms.cons1
-#             self.itms = self.itms.cons2()
-#             return itm1
+# # class strcon_iter:
+# #     def __iter__(self):
+# #         return self
+# #     def __init__(self, itms):
+# #         self.itms = itms
+# #     def __next__(self):
+# #         if (self.itms.ctag==0):
+# #             raise StopIteration
+# #         else:
+# #             itm1 = self.itms.cons1
+# #             self.itms = self.itms.cons2()
+# #             return itm1
 
-###########################################################################
+# ###########################################################################
 
-import sys 
-sys.path.append("./../../../../classlib/Python/")
-from MyPython import *
+# import sys 
+# sys.path.append("./../../../../classlib/Python/")
+# from MyPython import *
 
-def cube_sum(ij):
-  i = ij[0]
-  j = ij[1]
-  return i*i*i + j*j*j
+# def cube_sum(ij):
+#   i = ij[0]
+#   j = ij[1]
+#   return i*i*i + j*j*j
 
-def cube_sum_lte(ij1, ij2):
-  return (cube_sum(ij1) <= cube_sum(ij2))
+# def cube_sum_lte(ij1, ij2):
+#   return (cube_sum(ij1) <= cube_sum(ij2))
 
-def strcon_merge2(cxs1, cxs2, lte3):
-  cx1 = cxs1.cons1
-  cx2 = cxs2.cons1
-  if lte3(cx1, cx2):
-    return strcon_cons(cx1, lambda: strcon_merge2((cxs1.cons2)(), cxs2, lte3))
-  else:
-    return strcon_cons(cx2, lambda: strcon_merge2(cxs1, (cxs2.cons2)(), lte3))
+# def strcon_merge2(cxs1, cxs2, lte3):
+#   cx1 = cxs1.cons1
+#   cx2 = cxs2.cons1
+#   if lte3(cx1, cx2):
+#     return strcon_cons(cx1, lambda: strcon_merge2((cxs1.cons2)(), cxs2, lte3))
+#   else:
+#     return strcon_cons(cx2, lambda: strcon_merge2(cxs1, (cxs2.cons2)(), lte3))
   
-def stream_merge2(fxs1, fxs2, lte3):
-  return lambda: strcon_merge2(fxs1(), fxs2(), lte3)
+# def stream_merge2(fxs1, fxs2, lte3):
+#   return lambda: strcon_merge2(fxs1(), fxs2(), lte3)
 
-def generator_of_stream(fxs):
-  while True:
-    cxs = fxs()
-    if cxs.ctag == 0:
-      break
-    else:
-      fxs = cxs.cons2
-      yield cxs.cons1
-  raise StopIteration
+# def generator_of_stream(fxs):
+#   while True:
+#     cxs = fxs()
+#     if cxs.ctag == 0:
+#       break
+#     else:
+#       fxs = cxs.cons2
+#       yield cxs.cons1
+#   raise StopIteration
 
-def theNatPairs_cubesum():
-  def helper(i0):
-    return strcon_cons((i0, i0), stream_merge2(stream_tabulate(-1, lambda j0: (i0, i0+1+j0)), lambda: helper(i0+1), cube_sum_lte))
-  return generator_of_stream(lambda: helper(0))
+# def theNatPairs_cubesum():
+#   def helper(i0):
+#     return strcon_cons((i0, i0), stream_merge2(stream_tabulate(-1, lambda j0: (i0, i0+1+j0)), lambda: helper(i0+1), cube_sum_lte))
+#   return generator_of_stream(lambda: helper(0))
