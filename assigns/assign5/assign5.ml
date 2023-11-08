4b835152104e469f1b93e2b5b88e7709848a2180
Wyatt <wnapier@bu.edu>
2023-11-08 11:37:58 -0500

    assign5 updates

--- assigns/assign5/assign5.ml
+++ assigns/assign5/assign5.ml
@@ -1,4 +1,5 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
+#use "./../../lectures/lecture-11-02/code-2.ml";;
 (*
 
 Grammar (<expr> is the start symbol)
@@ -46,4 +47,41 @@ let rec trim cs =
    parse "((mul 1 2)" = None
 
 *)
+
+(*auxiliary functions*)
+
+let explode (s: string) : char list = 
+  let rec expl i l = 
+    if i < 0 then l 
+    else expl (i - 1) (String.get s i :: l) in 
+  expl (String.length s - 1) []
+
+(*end auxiliary functions*)
+
 let parse (s : string) : expr option = (* YOUR CODE *)
+  parse_expr (explode s)
+;;
+
+let parse_expr (c: char list) : expr option =
+
+;;
+
+let rec parse_exprs (c: char list) : exprs option =
+  match c with
+  | c1 ::  :: cs -> 
+;;
+
+(* let rec parse_num (c: char list) : int option =
+  match c with
+  | 
+  | [] ->  *)
+
+(* takes an int and needs to return an int option *)
+let parse_digit (c: char list) : int option =
+  if is_digit c then Some (int_of_char c - 48) else None
+
+
+  (* 
+  let t = int_of_char x - 48 in
+  if (0 <= t) && (t <= 9) then Some (Digit t) else None    
+  *)
