7d002c95ec24216b65ac7eb3784d5adee15e1b88
Wyatt <wnapier@bu.edu>
2023-11-10 15:01:54 -0500

    assign6 written

---
+++ assigns/assign6/MySolution/OCaml/assign6.ml
@@ -0,0 +1,76 @@
+(* ****** ****** *)
+(*
+//
+Assign6:
+Parsing and parsing combinators
+//
+DUE: the 13th of November, 2023
+//
+Except for the basic arithmetic functions
+(including those on chars), you may only use
+the functions in classlib/OCaml/MyOCaml.ml
+//
+*)
+(* ****** ****** *)
+
+(*
+//
+Assign6-1:
+//
+Please implement a print and parse function. Using parser combinators. When
+given a valid string according to the grammar, your parse function returns an
+sexpr value encoding the expression.
+
+//
+let sexpr_to_string (e : sexpr)  : string       = ...
+let sexpr_parse     (s : string) : sexpr option = ...
+//
+
+Example (Accepted Strings):
+parse "(add 1 2 3)" = Some (Add [Int 1; Int 2; Int 3])
+parse "(mul (add 1 2) 3 (mul 1))" = Some (Mul [Add [Int 1; Int 2]; Int 3; Mul [Int 1]])
+//
+Example (Rejected Strings):
+parse "()" = None
+parse "(add)" = None
+parse "(add 1 2))" = None
+parse "((mul 1 2)" = None
+//
+*)
+
+(* ****** ****** *)
+
+(*
+
+Grammar (<expr> is the start symbol)
+
+<digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+<num>   ::= <digit> | <digit><num>
+<exprs> ::= <expr> | <expr> <exprs>
+<expr>  ::= <num>
+          | (add <exprs> )
+          | (mul <exprs> )
+*)
+#use "./../../../../classlib/OCaml/MyOCaml.ml";;
+
+type sexpr =
+  | SInt of int        (* 1, 2, 3, 4 ...  *)
+  | SAdd of sexpr list (* (add e1 e2 ...) *)
+  | SMul of sexpr list (* (mul e1 e2 ...) *)
+
+(* let rec parser_expr *)
+let rec digit () = 
+  let* x = natural in 
+    (if (0<=x) && (x<=9) then pure (SInt x) else fail)
+;;
+
+let rec num () = 
+  
+
+let parse_expr e =
+  string_parse (digit ()) e
+
+
+(* ****** ****** *)
+
+(* end of [CS320-2023-Fall-assigns-assign6.ml] *)
