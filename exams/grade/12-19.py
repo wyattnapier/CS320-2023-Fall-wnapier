3abe7a7ae826f92691a62b47e4b87599c3a14f7f
Wyatt <wnapier@bu.edu>
2023-12-19 17:43:11 -0500

    calculating grade so far

---
+++ exams/grade/12-19.py
@@ -0,0 +1,47 @@
+# ####################################
+# Quiz1: Napier Wyatt James,wnapier@bu.edu,2,2,2,2,2,10,10,6,36,100,,
+# Quiz2: Napier Wyatt James,wnapier@bu.edu,0,0,3,2,2,1,1,0,0,33,,
+# Midterm: Napier Wyatt James,wnapier@bu.edu,3,3,3,0,3,1,0,1,1,3,1,3,1,1,1,1,1,3,3,3,1,4,0,10,51,75
+# Quiz3: Napier Wyatt James,wnapier@bu.edu,1,1,2,2,3,3,12,100,,
+# ####################################
+# wnapier@bu.edu
+# cs320-assign0-raw: 50.0
+# cs320-assign0-late: 100% // meaning late by 0 day(s)
+# cs320-assign1-raw: 90.0
+# cs320-assign1-late: 100% // meaning late by 0 day(s)
+# cs320-assign2-raw: 77.0
+# cs320-assign2-late: 100% // meaning late by 0 day(s)
+# cs320-assign3-raw: 80.0
+# cs320-assign3-late: 100% // meaning late by 0 day(s)
+# cs320-assign4-raw: 60.0
+# cs320-assign4-late: 100% // meaning late by 0 day(s)
+# ####################################
+wnapier_quiz1 = (100) * 4
+wnapier_quiz2 = (33 + 20) * 4
+wnapier_mterm = (75 + 20) * 12
+wnapier_hwpt1 = ((50 + 90 + 77 + 80 + 60) * 100 / 360) * 5
+# ####################################
+wnapier_part1 = (wnapier_quiz1 + wnapier_quiz2 + wnapier_mterm + wnapier_hwpt1) / 25
+print("wnapier_part1 =", wnapier_part1)
+# wnapier_part1 = 89.91333333333334
+# ####################################
+# Quiz4: Napier Wyatt James,wnapier@bu.edu,0,1,2,2,3,3,11,92,,
+wnapier_quiz4 = 92
+# ####################################
+wnapier_asgn6 = 50.0
+# ####################################
+wnapier_intp1 = 100.0
+# ####################################
+wnapier_intp2 = 100.0
+# ####################################
+wnapier_intp3 = 100.0
+# ####################################
+wnapier_asgn5 = 25.0+25.0
+# ####################################
+wnapier_quiz3 = 100
+# ####################################
+wnapier_asgn6_2 = 0
+wnapier_part2 = (4+0.02*wnapier_asgn5+0.04*wnapier_asgn6+0.04*wnapier_asgn6_2+0.334*12/100*wnapier_quiz3+0.334*12/100*wnapier_quiz4+0.08*wnapier_intp1+0.08*wnapier_intp2+0.08*wnapier_intp3)*2.5
+print("wnapier_part2 =", wnapier_part2)
+# print("everything except final =", wnapier_part1 * 0.3 + wnapier_part2 * 0.4 + _____ * 0.3)
+# ####################################
